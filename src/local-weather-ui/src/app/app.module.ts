744854df231e442fc7f1cc6681478e02dee09093
Aleksandr Maha <maha.aleksandr@apriorit.com>
2017-05-16 12:42:10 +0300

    add localweather.component

--- src/local-weather-ui/src/app/app.module.ts
+++ src/local-weather-ui/src/app/app.module.ts
@@ -4,17 +4,21 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { LocalweatherComponent } from './weather/components/localweather/localweather.component';
+
+import { WeatherService } from "./weather/services/weather.service";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LocalweatherComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [WeatherService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/local-weather-ui/src/app/weather/services/weather.service.ts
+++ src/local-weather-ui/src/app/weather/services/weather.service.ts
@@ -1,14 +1,27 @@
 import { Injectable } from '@angular/core';
 import { Http } from "@angular/http";
 
+import { BaseApiService } from "./base-api.service";
 import { WeatherParameter } from "../models/weather-parameter.model";
 
-@Injectable()
-export class WeatherService {
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
-    constructor(http: Http) { }
+@Injectable()
+export class WeatherService extends BaseApiService {
+    baseUrl: string = "http://localhost:61865/"
+
+    constructor(private http: Http) {
+        super();
+    }
 
     getWeather(param: WeatherParameter) {
+        return this.http.get(this.createRequestUrl(param))
+            .map(this.extractData)
+            .catch(this.handleError);
+    }
 
+    private createRequestUrl(param: WeatherParameter): string {
+        return this.baseUrl + "api/weather?" + "RequestType=" + param.RequestType + "&Lat=" + param.Lat + "&Lon=" + param.Lon;
     }
 }
